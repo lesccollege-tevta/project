77bb8da426e55e56133d305d58c88394fe2474cf
lesc <lesc@gmail.com>
2026-09-09 15:34:16 +0500

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+</head>
+<body>
+    <h1>PRoject</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, molestiae!</p>
+</body>
+</html>
