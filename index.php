865b717df9b49195efdb5207b4aeea768deb8929
lesc <lesc@gmail.com>
2026-09-09 15:42:31 +0500

    Add

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project</title>
-</head>
-<body>
-    <h1>PRoject</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, molestiae!</p>
-     <h2>Welcome</h2>
-</body>
-</html>
+<?php
+echo "Welcome ";
+?>
